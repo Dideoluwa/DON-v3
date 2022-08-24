9d730419ddbe4e5274f8fba947a8fcd7acc60fec
Dideoluwa Oni <darasimidideoluwa@gmail.com>
2022-08-24 13:05:04 +0100

    Added time to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './About.module.css'
 import NavBar from './NavBar'
 import img from '../Assets/IMG_4856.jpg'
 
 function About() {
+
+    let [time, setTime] = useState([])
+    // let date = new Date();
+    // let h = date.getHours();
+    // let s = date.getSeconds()
+    // let m = date.getMinutes();
+    // h = (h < 10) ? "0" + h : h;
+    // m = (m < 10) ? "0" + m : m;
+    // let newdate = h + ':' + m + ':' + s;
+    // // setTime(newdate)
+    // useEffect(() => {
+    //     setTime(newdate)
+    // }, [newdate])
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            let date = new Date()
+            let h = date.toLocaleString('en-US', { hour: 'numeric', hour12: false, timeZone: 'Africa/Lagos' });
+            let s = date.toLocaleString('en-US', { second: '2-digit', timeZone: 'Africa/Lagos' });
+            let m = date.toLocaleString('en-US', { minute: '2-digit', timeZone: 'Africa/Lagos' });
+            m = (m < 10) ? "0" + m : m;
+            s = (s < 10) ? "0" + s : s;
+            setTime(`${h}:${m}:${s}`);
+        }, 1000);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
     return (
         <div>
             <NavBar />
@@ -12,7 +41,7 @@ function About() {
                     <h1>ABOUT</h1>
                 </div>
                 <div className={styles.about_imageWrapper}>
-                    <img src={img} alt= 'Darasimi'/>
+                    <img src={img} alt='Darasimi' />
                 </div>
                 <div className={styles.test}>
                     <p>
@@ -29,12 +58,17 @@ function About() {
                         and <span className={styles.span}><a href='https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX5mILnBJLA26?si=tfL8-88fS6y5w-i_I3OXNA' target='_blank' rel="noreferrer"> Amapiano</a></span>.
                         I love the <span className={styles.w3}>log drums</span><span className={styles.emoji3}>🪘</span>.
                     </p>
+                    <div className={styles.about_time}>
+                        <h2>Location: LAGOS</h2>
+                        <h2>Time: {time}</h2>
+                    </div>
+
                 </div>
                 <div className={styles.body_child}>
                     <h1>MY SKILLS</h1>
                 </div>
                 <div className={styles.skills}>
-                   
+
                 </div>
             </div>
         </div>
