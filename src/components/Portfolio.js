0a2bb6b3f44333c117bb50ffe576bbf670f1f674
Dideoluwa Oni <darasimidideoluwa@gmail.com>
2022-09-05 03:43:22 +0100

    portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,9 @@
 import React from 'react'
 // import Footer from './Footer'
+import { Link } from 'react-router-dom'
 import NavBar from './NavBar'
 import styles from './Portfolio.module.css'
+import Lists from './PortfolioList'
 
 function Portfolio() {
     return (
@@ -10,6 +12,20 @@ function Portfolio() {
             <div className={styles.body}>
                 <div className={styles.body_child}>
                     <h1>PORTFOLIO</h1>
+                </div>
+                <div className={styles.portfolio_list}>
+                    {Lists.map((list) => (
+                        <div className={styles.card}>
+                            <Link to={`/description/${list.id}`}>
+                                <div key={list.id}>
+                                    <div className={styles.img_wrapper}>
+                                        <img src={list.img} alt={list.title} />
+                                    </div>
+                                    <h2>{list.title}</h2>
+                                </div>
+                            </Link>
+                        </div>
+                    ))}
                 </div>
             </div>
             {/* <Footer /> */}
---
+++ src/components/PortfolioList.js
@@ -0,0 +1,19 @@
+import jobify from '/Users/mac/Desktop/Darasimi files/DON-v3/src/Assets/projectImages/jobify.png'
+import news from '/Users/mac/Desktop/Darasimi files/DON-v3/src/Assets/projectImages/news-update.png'
+import emmanuel from '/Users/mac/Desktop/Darasimi files/DON-v3/src/Assets/projectImages/Emmanuel odudu.png'
+const Lists = [
+    {
+        id: 1,
+        title: "Jobify",
+        description: "This is a hackathon project which I worked as the sole Frontend developer on my team.We built this application to enhance collaboration between people remotely and also assist freelancers in getting gigs and also giving out gigs.It is a very user friendly application that anybody can use.",
+        img: emmanuel,
+    },
+    {
+        id: 2,
+        title: "News-Update App",
+        description: "This is a hackathon project which I worked as the sole Frontend developer on my team.We built this application to enhance collaboration between people remotely and also assist freelancers in getting gigs and also giving out gigs.It is a very user friendly application that anybody can use.",
+        img: emmanuel,
+    },
+];
+
+export default Lists
